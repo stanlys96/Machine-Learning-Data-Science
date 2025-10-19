4074994ddda087544702bc01488324994038160d
stanlys96 <stanlyskwok@gmail.com>
2025-10-19 17:32:11 +0700

    first commit

---
+++ IBM Data Science/Course 6 - Databases and SQL for Data Science with Python/Module 6 - Advanced SQL for Data Engineers/sql/transaction_james.sql
@@ -0,0 +1,27 @@
+DELIMITER //
+
+CREATE PROCEDURE TRANSACTION_JAMES()
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+    	ROLLBACK;
+        RESIGNAL;
+    END;
+    START TRANSACTION;
+    
+    UPDATE BankAccounts
+    SET Balance = Balance - 1200
+    WHERE AccountName = 'James';
+    
+    UPDATE BankAccounts
+    SET Balance = Balance + 1200
+    WHERE AccountName = 'Shoe Shop';
+    
+    UPDATE ShoeShop
+    SET Stock = Stock - 4
+    WHERE Product = 'Trainers';
+    
+    COMMIT;
+END //
+
+DELIMITER ;
